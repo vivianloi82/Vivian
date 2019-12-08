2564bf32bd23a7753216cb83ec003d3c74961461
vivianloi82 <vivianljt-wa16@student.tarc.edu.my>
2019-12-09 01:10:12 +0800

    Profile not yet done

--- Profile/BrowseRewards.js
+++ Profile/BrowseRewards.js
@@ -1,82 +1,82 @@
-// //This is an example code for Bottom Navigation//
-// import React from 'react';
-// //import react in our code.
-// import { Text, View, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
-// //import all the basic component we have used
-// import Fire from "../Fire";
+//This is an example code for Bottom Navigation//
+import React from 'react';
+//import react in our code.
+import { Text, View, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+//import all the basic component we have used
+import Fire from "../Fire";
 
-// export default class BrowseRewardsScreen extends React.Component {
+export default class BrowseRewardsScreen extends React.Component {
 
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             rewards: {
-//                 category: '',
-//                 points: '',
-//                 title: ''
-//             }
+    constructor(props) {
+        super(props);
+        this.state = {
+            rewards: {
+                category: '',
+                points: '',
+                title: ''
+            }
 
-//         };
-//     }
-
-
-
-//     componentDidMount() {
-//         Fire.shared.firestore.collection("BrowseRewards")
-//             .get()
-//             .then(snapshot => {
-//                 snapshot.forEach(doc => {
-//                     console.log(doc.data());
-//                     this.setState({
-//                         rewards: {
-//                             category: doc.data().category,
-//                             points: doc.data().points,
-//                             title: doc.data().title,
-//                         }
-
-
-//                     })
-//                 });
-
-
-//             })
-//             .catch(err => {
-//                 console.log('Error getting document', err);
-
-//             });
-//     }
+        };
+    }
 
 
 
-//     renderRewards = () => {
-//         return (
-//             <View>
-//                 <Text>
-//                     {this.state.rewards.category}
-//                 </Text>
-//                 <Text>
-//                     {this.state.rewards.title}
-//                 </Text>
-//                 <Text>
-//                     {this.state.rewards.points}
-//                 </Text>
-
-//             </View>
-//         );
-//     };
-//     render() {
-//         return (
-//             <View style={{ flex: 1 }}>
-
-//                 <FlatList
-//                     data={rewards}
-//                     renderItem={({ item }) => this.renderRewards(item)}
+    componentDidMount() {
+        Fire.shared.firestore.collection("BrowseRewards")
+            .get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    console.log(doc.data());
+                    this.setState({
+                        rewards: {
+                            category: doc.data().category,
+                            points: doc.data().points,
+                            title: doc.data().title,
+                        }
 
 
-//                 />
+                    })
+                });
 
 
-//             </View>
-//         );
-//     }
-// }
+            })
+            .catch(err => {
+                console.log('Error getting document', err);
+
+            });
+    }
+
+
+
+    renderRewards = () => {
+        return (
+            <View>
+                <Text>
+                    {this.state.rewards.category}
+                </Text>
+                <Text>
+                    {this.state.rewards.title}
+                </Text>
+                <Text>
+                    {this.state.rewards.points}
+                </Text>
+
+            </View>
+        );
+    };
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+
+                <FlatList
+                    data={rewards}
+                    renderItem={({ item }) => this.renderRewards(item)}
+
+
+                />
+
+
+            </View>
+        );
+    }
+}
